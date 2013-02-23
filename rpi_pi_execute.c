ce6b1a389f5df6144e308efbfccce0d6315faf2b
tetrep <paul@blacksheepwall.com>
2013-02-23 12:24:01 -0500

    not sure how i want to proceed at the moment, trying to make a function agnostic wrapper to fork any command

--- rpi_pi_execute.c
+++ rpi_pi_execute.c
@@ -3,22 +3,65 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#define RPI_PI_FIFO_PATH "super_dooper_awesome_fifo"
+
+//forks on the given function and redirects its stdout to us
+int rpi_pi_forker(void *fp, void *arg)
+{
+  //pid of our child
+  pid_t child_pid = -1;
+
+  //can we open our fifo?
+  if(freopen(RPI_PI_FIFO_PATH, "r", stdin) == NULL)
+  {
+    //fifo might not exist, so make it and try again
+    if(mkfifo(RPI_PI_FIFO_PATH, 0660) != 0 || freopen(RPI_PI_FIFO_PATH, "r", stdin) == NULL)
+      return -1;
+  }
+
+  //fork it!
+  child_pid = fork();
+
+  //did it fork?
+  switch(child_pid)
+  {
+    //we failed
+    case -1:
+      return -1;
+      break;
+
+    //we are a baby
+    case 0:
+      fp(arg);
+      break;
+
+    //we made a baby
+    default:
+      break;
+  }
+
+}
 
 int rpi_pi_lpq(char* user, char* printer)
 {
-    //plus 2 because we need to end the command with ' and \0
-    char cmd[strlen("su ") + strlen(user) + strlen(" -c 'lpq -lP") + strlen(printer) + 2];
-    //build our command
-    cmd[0] = '\0';
-    strcat(cmd, "su ");
-    strcat(cmd, user);
-    strcat(cmd, " -c 'lpq -lP ");
-    strcat(cmd, printer);
-    strcat(cmd, "'");
+  //plus 2 because we need to end the command with ' and \0
+  char cmd[strlen("su ") + strlen(user) + strlen(" -c 'lpq -lP") + strlen(printer) + 2];
+  //build our command
+  cmd[0] = '\0';
+  strcat(cmd, "su ");
+  strcat(cmd, user);
+  strcat(cmd, " -c 'lpq -lP ");
+  strcat(cmd, printer);
+  strcat(cmd, "'");
 
-    //return system(cmd);
-    printf("%s\n", cmd);
-    return 0;
+  //return system(cmd);
+  printf("%s\n", cmd);
+  return 0;
 }
 
 int rpi_pi_lpr(char* user, char* printer, void* file)
--- rpi_pi_execute.h
+++ rpi_pi_execute.h
@@ -6,15 +6,20 @@
   //@param user the user that will run this command
   //@param printer the printer we wish to view the queue of
   int rpi_pi_lpq(char* /*user*/, char* /*printer*/);
+
   //@brief prints a given file to a given printer
   //@param user the user that will run this command
   //@param printer the printer we wish to send the file to
   //@param file the file we wish to print
   int rpi_pi_lpr(char* /*user*/, char* /*printer*/, void* /*file*/);
+
   //@brief cancels a given job from a given printer
   //@param printer the printer that has the job we want to cancel
   //@param job the job we want to cancel
   int rpi_pi_lprm(char* /*user*/, char* /*printer*/, char* /*job*/);
+
+  //@brief sets up the pipefd
+  int rpi_pi_pipe_setup();
 
   //short function names
   #ifdef RPI_PI_EXECUTE_SHORT_NAMES
