478c318640c5c3535991b04ecf931d5242a164fc
tetrep <paul@blacksheepwall.com>
2013-05-08 15:39:28 -0400

    static linked it, hopefully it will run on the server now

--- cgi_build_and_test.sh
+++ cgi_build_and_test.sh
@@ -6,9 +6,12 @@ rm -fv /cgi/mime.cgi &&
 #we want extra warnings
 #we want warnings to be errors
 #we want the sexy c++ standard
+#we want to statically link the standard library
 #we want to print all the errors
-#we want to write everything to a log file
-clang++ -Wall -Wextra -Werror -std=c++11 -ferror-limit=0 mime.cpp parse_cgi.cpp -o mime.cgi &> cgi_build_log.txt &&
+clang++ -Wall -Wextra -Werror -std=c++11 -static-libstdc++ -ferror-limit=0 mime.cpp parse_cgi.cpp &> cgi_build_log.txt &&
+
+#got the libraries from ldd
+#clang++ -Wall -Wextra -Werror -std=c++11 -ferror-limit=0 -l libstdc++.so.6 -l libm.so.6 -l libgcc_s.so.1 -l libc.so.6 mime.o parse_cgi.o -o mime.cgi &&
 
 #copy our binary to the cgi directory
 #cp /cgi/rpi_pi/mime.cgi /cgi/ &&
@@ -27,9 +30,9 @@ clear &&
 #let's test GET
   export REQUEST_METHOD=GET &&
   export QUERY_STRING=${url_encoded} &&
-  echo "${url_encoded}" | ./parse_cgi.out &&
+  echo "${url_encoded}" | ./mime.cgi &&
 
 #let's test POST
   export REQUEST_METHOD=POST &&
   export CONTENT_LENGTH=${#url_encoded} &&
-  echo "${url_encoded}" | ./parse_cgi.out;
+  echo "${url_encoded}" | ./mime.cgi;
