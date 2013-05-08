56622583bf87a0d9446dddec313351a137a3e26f
tetrep <paul@blacksheepwall.com>
2013-05-08 15:53:38 -0400

    need to static link more than just the standard library, back to manual method

--- cgi_build_and_test.sh
+++ cgi_build_and_test.sh
@@ -6,9 +6,9 @@ rm -fv /cgi/mime.cgi &&
 #we want extra warnings
 #we want warnings to be errors
 #we want the sexy c++ standard
-#we want to statically link the standard library
 #we want to print all the errors
-clang++ -Wall -Wextra -Werror -std=c++11 -static-libstdc++ -ferror-limit=0 mime.cpp parse_cgi.cpp -o mime.cgi &> cgi_build_log.txt &&
+#we just want to compile
+clang++ -Wall -Wextra -Werror -std=c++11 -ferror-limit=0 -c mime.cpp parse_cgi.cpp &> cgi_build_log.txt &&
 
 #got the libraries from ldd
 #clang++ -Wall -Wextra -Werror -std=c++11 -ferror-limit=0 -l libstdc++.so.6 -l libm.so.6 -l libgcc_s.so.1 -l libc.so.6 mime.o parse_cgi.o -o mime.cgi &&
