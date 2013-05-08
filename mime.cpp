5d0ada7998b8ae357817f63ff91649157296a13e
tetrep <paul@blacksheepwall.com>
2013-05-08 14:38:57 -0400

    forgot to actually make it valid cgi

--- mime.cpp
+++ mime.cpp
@@ -3,6 +3,29 @@
 #include <typeinfo> //for typeid
 #include <utility> //for std::pair
 
+//@brief outputs what we need for valid http
+void http_output(std::string output)
+{
+  //we need to print this first
+  std::cout << "Content-type: text/html" << std::endl
+            //it's html!
+            << "<html>" << std::endl
+              //our title
+              << "<title>TEST PAGE</title>" << std::endl
+              //start of the body
+              << "<body>" << std::endl
+                //don't format our given output
+                << "<code>" << std::endl
+                  //actually print what we were given
+                  << output << std::endl
+                //close the code tag
+                << "</code>"
+              //close the body tag
+              << "</body>" << std::endl
+            //close the html tag
+            << "</html>" << std::endl;
+}
+
 //@brief we wan't to repeat the key-value pairs we read in and the command output
 int main()
 {
@@ -21,11 +44,10 @@ int main()
   }
   catch(const std::exception &e)
   {
-    //oh noes!
-    std::cout << "=>" << typeid(e).name() << std::endl << "-->" << e.what() << std::endl;
+    http_output(typeid(e).name() + std::string("\n") + e.what());
 
     //we don't discriminate errors...
-    return 1;
+    return 0;
   }
   return 0;
 }
